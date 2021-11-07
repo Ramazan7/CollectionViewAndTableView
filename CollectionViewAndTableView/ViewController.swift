8ac6dac520285d87cbdcb23681d4eada008a9337
TheRamazan <ramazan05.00@mail.ru>
2021-11-07 19:06:47 +0300

    Initial Commit

---
+++ CollectionViewAndTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewAndTableView
+//
+//  Created by Ramazan on 07.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
